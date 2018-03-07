df2d65aff89eace9edc15b92bf22881380f28da3
nadelinageorgieva <nadelinageorigeva@hotmail.com>
2018-03-07 09:24:52 -0500

    All posts pages layout
    I added animation to the page and changed the layout of my posts, currently opened posts and solved posts

--- resources/views/posts/list_posts.blade.php
+++ resources/views/posts/list_posts.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+    <!-- Styles -->
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+
+    <div class="container">
     @if (session('status'))
     <div class="alert alert-success">
         {{ session('status') }}
@@ -12,16 +18,59 @@
     </br>
 
     @foreach($posts as $post)
-    <h3><a href="{{route('post.show', ['post' => $post->id])}}">{{ $post->title }}</a></h3>
-    <p>{{ $post->body }}</p>
-    <p>Last Edit: {{mb_substr($post->updated_at, 0, 10)}}</p>
-    <h3>Comments</h3>
 
-        @foreach($post->comments as $comment)
-            <h4>{{$comment->name}} commented: </h4>
-            <p>{{$comment->comment}}</p>
-            <p>Last Edit: {{mb_substr($comment->updated_at, 0, 10)}}</p>
-        @endforeach
+
+
+        <?php
+            $numberOfComments = count($post->comments);
+            $numberOfViews = $post->view_count;
+        ?>
+            <div class ="row" >
+                <div class="col-sm-1">
+                    <span class = "nb-of-Comments-Block "style=   " overflow: auto;
+                          WIDTH: 100%;
+                          display: block;
+                          text-align: center;">
+                        <a href="{{route('post.show', ['post' => $post->id])}}">{{$numberOfComments }}</a>
+                        </br>
+                    Comments</span>
+                </div>
+
+                <div class="col-sm-1">
+                    <span style=   " overflow: auto;
+                          WIDTH: 100%;
+                          display: block;
+                          text-align: center;">
+                        <a href="{{route('post.show', ['post' => $post->id])}}">N/A</a>
+                        </br>
+                        Votes</span>
+                </div>
+
+                <div class="col-sm-1">
+                    <span class = "nb-of-Comments-Block "style=   " overflow: auto;
+                          WIDTH: 100%;
+                          display: block;
+                          text-align: center;">
+                        <a href="{{route('post.show', ['post' => $post->id])}}">{{$numberOfViews }}</a>
+                        </br>
+                        Views</span>
+                </div>
+
+                <div class="col-sm-9" >
+                    <span style=   " overflow: auto;
+                          WIDTH: 100%;
+                          display: block;
+                          text-align: center;">
+
+                    <a href="{{route('post.show', ['post' => $post->id])}}">{{ $post->title }}</a>
+                    <p>Last Edit: {{mb_substr($post->updated_at, 0, 10)}}</p>
+                        </span>
+                </div>
+            </div>
+
+
+
     @endforeach
+
 </div>
 @endsection
