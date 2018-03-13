0610c1a0baa81040bee4722437b492b0f33c08eb
Your Name <you@example.com>
2018-03-13 15:03:50 -0400

    Changes

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,26 +20,37 @@ class ProfileController extends Controller
     public function addProfile(Request $request){
     	$this ->validate($request,[
     		'name' => 'required',
-    	    'title' => 'required',
-    	    'profile_pic' => 'required',
+    	  'title' => 'required',
+    	  'profile_pic' => 'required',
     	]);
 
     	$profiles = new Profile;
-        $profiles ->user_id = Auth::user()->id;
+      $profiles ->user_id = Auth::user()->id;
     	$profiles ->name = $request->input('name');
-    	$profiles ->designation = $request->input('title');
+    	$profiles ->title = $request->input('title');
     	if(Input::hasFile('profile_pic'))
     		{
-    		$file = Input::file('profile_pic');
-    		$file->move(public_path().'/uploads/',$file->getClientOriginalName());
-    		$url =URL::to("/").'/uploads/' .$file->getClientOriginalName();
+    	 $file = Input::file('profile_pic');
+    	 $file->move(public_path().'/uploads/',$profiles->user_id.'.'.$file->getClientOriginalExtension()); 
+    	 $url =URL::to("/").'/uploads/'.$profiles->user_id.'.'.$file->getClientOriginalExtension();
     	    }
+            $profiles ->profile_pic = $url;
 
 
-    	$profiles ->profile_pic = $url;
+       $profileCheck = Profile::find($profiles->user_id);
+       if($profileCheck!=null){
+           $profileCheck ->name = $profiles -> name;
+           $profileCheck ->title = $profiles -> title;
+           $profileCheck ->profile_pic= $profiles -> profile_pic;
+           $profileCheck ->save();
+           return redirect('/home')->
+           with('response', 'profile was succesfully updated');
+         }
+        else{
     	$profiles -> save();
     	return redirect('/home')->
     	with('response','Profile Added Successfully');
+        }
 
        }
 }
