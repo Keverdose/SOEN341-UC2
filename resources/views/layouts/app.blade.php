56c5ed88dfff7c16ec5de888aaf4aff330cfad8b
dminkin <35669560+dminkin@users.noreply.github.com>
2018-03-17 15:08:06 -0400

    Changes to Navbar
    Removed the home link from the navbar and put a link for viewing the solved posts instead.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,9 +44,9 @@
                     <!-- Right Side Of Navbar -->
                     <div class="top-right linkstwo">
                         @auth
-                        <a href="{{ url('/') }}">Home</a>
                         <a href="{{ url('/post/create') }}">Create a Post</a>
                         <a href="{{ url('/open/posts') }}">View Open Posts</a>
+                        <a href="{{ url('/solved/posts') }}">View Solved Posts</a>
                         <a href="{{ url( route('user.activity', ['user_id' => Auth::id()])) }}">My Posts</a>
                         <a href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
