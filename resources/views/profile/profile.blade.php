6bea049579f97ba9f3d067f268e6107ee0f0f9f9
Your Name <you@example.com>
2018-03-21 16:13:10 -0400

    Displays more information
    Displays number of posts and number of best answers for user

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,53 +1,38 @@
 @extends('layouts.app')
-<style type = "text/css">
-    .picture{
-            border-radius: 150%;
-            width:150px;
-            height:150px;}
-    
-    .group{ position: relative;left: 10%;
-          font-family: "Times New Roman", Times, serif;;}
-
-    .user_info{position: relative;left: 10%;
-                font: italic bold 12px/30px Georgia, serif}
-
-    #user_name{ font-size: large;}
-
-    #profile{font-size: large;
-            }
-</style>
 @section('content')
 
+<div class="container ">
+    <div class="col-sm-11">
+  
 
-
-
-
-
-                  <div class= "group">
-                   @if(!empty($user->profile_pic))
-                        <img src ="{{$user->profile_pic}}" class = "picture" alt="" style="float:left">
-                        @else
-                        <img src ="{{url('images/avatar.jpg')}}" class = "picture" alt="" style= "float:right">
-                   @endif
-
-
-
-              
-                   <ul class = user_info style="list-style-type:none">
-                    <li id = user_name>{{$user->user_name}}</li>
-                    <li id = title>{{$user->title}}</li>
+                 <ul class = "user_stats" style="list-style-type:none">  
                     <li>Member since: {{$time}}</li>
-                    <li>{{$user->view_count}} profile view</li>
+                    <li>{{$user->view_count}} profile views</li>
+                    <li>Number of posts: {{count($posts)}}</li>
+                    <li>Number of answers: {{count($comments)}}</li>
+                 </ul>
+
+
                     @if($user->id == Auth::user()->id)
-                    <li><a href= "{{ route('editProfile') }}" id= profile>Edit Profile</a></li>
+                    <a href= "{{ route('editProfile') }}"id= profile>Edit Profile</a>
                     @endif
-                   </ul>
-                 </div>
 
 
-                   
-                </div>
-            </div>
-        </div>
+                    @if(!empty($user->profile_pic))
+                    <img src ="{{$user->profile_pic}}" class = "picture" alt="" style="float:left">
+                    @else
+                    <img src ="{{url('images/avatar.jpg')}}" class = "picture" alt="" style= "float:left">
+                    @endif
+
+
+                  <ul class = user_info style="list-style-type:none">
+                    <li id = user_name>User name:  {{$user->user_name}}</li>
+                    <li id = title>Title:  {{$user->title}}</li>
+                    <li>About me:</li>
+                    <li><pre id="about_me">{{$user->about_me}}</pre></li>
+                 </ul>
+          
+   </div>
+</div>
 
 @endsection
