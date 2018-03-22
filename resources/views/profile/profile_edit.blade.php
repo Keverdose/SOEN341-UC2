35a4c7d347610e1529d0be2fa2127c7547ab5de5
Your Name <you@example.com>
2018-03-22 15:14:53 -0400

    About me
    About me html for editing profile view

--- resources/views/profile/profile_edit.blade.php
+++ resources/views/profile/profile_edit.blade.php
@@ -31,7 +31,7 @@
                             <label for="user_name" class="col-md-4 control-label">User Name</label>
 
                             <div class="col-md-6">
-                                <input id="user_name" type="name" class="form-control" name="user_name" value="{{ old('user_name') }}">
+                                <input id="user_name" type="name" class="form-control" name="user_name" maxlength ="12" value= "{{$user->user_name}}">
 
                                 @if ($errors->has('user_name'))
                                     <span class="help-block">
@@ -49,7 +49,7 @@
                             <label for="title" class="col-md-4 control-label">Title</label>
 
                             <div class="col-md-6">
-                                <input id="title" type="input" class="form-control" name="title">
+                                <input id="title" type="input" class="form-control" name="title" maxlength ="20" value= "{{$user->title}}">
 
                                 @if ($errors->has('title'))
                                     <span class="help-block">
@@ -65,7 +65,8 @@
                          <div class="form-group{{ $errors->has('profile_pic') ? ' has-error' : '' }}">
                             <label for="profile_pic" class="col-md-4 control-label">Profile picture</label>
                               <div class="col-md-6">
-                                <input id="profile_pic" type="file" class="form-control" name="profile_pic">
+                                <input id="profile_pic" type="file" class="form-control" name="profile_pic"
+                                value="{{$user->profile_pic}}">
 
                                 @if ($errors->has('profile_pic'))
                                     <span class="help-block">
@@ -74,6 +75,24 @@
                                 @endif
                              </div>
                          </div>
+
+
+
+
+                         <div class="form-group{{ $errors->has('about_me') ? ' has-error' : '' }}"> 
+                           <label for="about_me" class="col-md-4 control-label">About me
+                            </label>
+                              <div class="col-md-6"> 
+                                <pre><textarea name="about_me" id="about_me" cols="30" rows="10" class="form-control" maxlength="400" wrap="hard">{{$user->about_me}}</textarea></pre>
+                                @if ($errors->has('about_me'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('about_me') }}</strong>
+                                    </span>
+                                @endif
+                             </div>
+                         </div>
+
+
                
 
                         <div class = "form-group">
