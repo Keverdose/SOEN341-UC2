0fd5aa17fcd6aa1f6b93d3fa1eee8f4f9e74ab6d
Keverdose <yekevin1995@hotmail.com>
2018-03-23 11:00:34 -0400

    Added test for tag creation and linking to post

--- tests/Unit/TagTest.php
+++ tests/Unit/TagTest.php
@@ -2,24 +2,29 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+use App\Tag;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class TagTest extends TestCase
 {
+    use DatabaseMigrations;
     /**
      * Testing for tag creation.
      *
      * @return void
      */
-    public function testExample(){
-        factory(Post::class)->create([
-            'title' => 'test title',
-            'body' => 'test body',
-            'category_id' => 1,
-            'solved' => FALSE
+    public function test_tags(){
+        $post = factory(Post::class)->create();
+        factory(Tag::class)->create([
+            'name' => 'Test'
         ]);
-        $this->assertDatabaseHas('posts', ['body' => 'test body']);
+        $this->assertDatabaseHas('tags', ['name' => 'test']);
+        $this->assertNull($post->tags()->first());
+        $post->tags()->sync($post,false);
+        $this->assertNotNull($post->tags()->first());
     }
 }
